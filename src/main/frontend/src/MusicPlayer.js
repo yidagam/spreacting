61015ad3b0eaf6df00b3c161cf2ccfb1f57bb360
yidagam <yidagam@gmail.com>
2026-06-18 13:43:42 +0900

    css 추가(수정필), console.log 주석화

--- src/main/frontend/src/MusicPlayer.js
+++ src/main/frontend/src/MusicPlayer.js
@@ -6,25 +6,26 @@ import YouTube from "react-youtube";
 export default function MusicPlayer({ videoId }) {
   const playerOpened = async () => {
     // 재생 중인 노래 isPlaying=true 설정
+    // eslint-disable-next-line no-unused-vars
     let res = await axios({
       method: "Post",
       url: "http://localhost:8080/musicplayer/open",
       data: { video_id: videoId },
     });
 
-    console.log(res);
+    // console.log(res);
   };
 
   const playerClosed = async () => {
     // 다음노래 재생
-
+    // eslint-disable-next-line no-unused-vars
     let res = await axios({
       method: "Post",
       url: "http://localhost:8080/musicplayer/close",
       data: { video_id: videoId },
     });
 
-    console.log(res);
+    // console.log(res);
   };
 
   const opts = {
